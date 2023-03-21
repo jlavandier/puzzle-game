9737197b31d11aa5033bf4659b28c8aef5555d80
Javier Lavandier Tejada <javierlavandier@gmail.com>
2023-03-21 14:33:05 -0400

    Puzzle-Game

---
+++ README.md
@@ -0,0 +1,7 @@
+### Puzzle Game
+
+- Demo: 
+
+- This puzzle do the next things, write click handlers to drag and drop puzzle pieces onto the board, and keep track of the number of turns the player has taken to solve the puzzle.
+
+- Is customizable, in the code you only have to change the image you going to use and the rows and columns you must need.
---
+++ js/index.js
@@ -0,0 +1,84 @@
+let rows = 4;
+let columns = 4;
+let currTitle;
+let otherTitle;
+let turns = 0;
+
+window.onload = function () {
+    for(let i = 0; i < rows; i++){
+        for (let i = 0; i < columns; i++) {
+            let tile = document.createElement("img");
+            tile.src = "../assets/Blank.png";
+
+            tile.addEventListener("dragstart", dragStart); //click on image to drag
+            tile.addEventListener("dragover", dragOver);   //drag an image
+            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+
+            document.getElementById("board").append(tile)
+        }
+    }
+
+    let pieces = [];
+    for(let i=1; i <= rows*columns; i++) {
+        pieces.push(i.toString());
+    }
+    pieces.reverse();
+    for(let i = 0; i < pieces.length; i++) {
+        let j = Math.floor(Math.random() * pieces.length);
+
+        let tmp = pieces[i];
+        pieces[i] = pieces[j];
+        pieces[j] = tmp;
+    }
+
+
+    for(let i = 0; i < pieces.length; i++) {
+        let tile = document.createElement("img");
+        tile.src = `../assets/${pieces[i]}.jpg`
+        tile.addEventListener("dragstart", dragStart); //click on image to drag
+        tile.addEventListener("dragover", dragOver);   //drag an image
+        tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+        tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+        tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+        tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+        document.getElementById("pieces").append(tile)
+    }
+
+}
+
+function dragStart() {
+    currTile = this; //this refers to image that was clicked on for dragging
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; //this refers to image that is being dropped on
+}
+
+function dragEnd() {
+    if (currTile.src.includes("blank")) {
+        return;
+    }
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+}
+
